2c1af64036274b3a2a3f99d5ca251c10992e846e
Bioray-Nathan <nlee@bioray.com>
2024-10-25 09:45:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,3 +32,9 @@ hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
 st.bar_chart(hist_values)
+
+# Plot data on a map
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
